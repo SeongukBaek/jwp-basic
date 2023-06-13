871b6f94528487d4793b63194e59bd2d161198a8
SeongukBaek <bsu6235@gmail.com>
2023-06-13 17:23:22 +0900

    feat: reflections

--- src/test/java/core/ref/ReflectionTest.java
+++ src/test/java/core/ref/ReflectionTest.java
@@ -7,6 +7,10 @@ import org.slf4j.LoggerFactory;
 import next.model.Question;
 import next.model.User;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
@@ -14,12 +18,23 @@ public class ReflectionTest {
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+
+        logger.debug(Arrays.toString(clazz.getDeclaredFields()));
+        logger.debug(Arrays.toString(clazz.getDeclaredConstructors()));
+        logger.debug(Arrays.toString(clazz.getDeclaredMethods()));
     }
     
     @Test
-    public void newInstanceWithConstructorArgs() {
+    public void newInstanceWithConstructorArgs() throws Exception {
         Class<User> clazz = User.class;
         logger.debug(clazz.getName());
+        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+
+        for (Constructor<?> constructor : constructors) {
+            constructor.newInstance("ssafy", "ssafy", "bsu", "bsu@ssafy.com");
+        }
+
+
     }
     
     @Test
